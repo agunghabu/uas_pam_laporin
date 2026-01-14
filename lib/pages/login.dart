4e09185e671d9996a5827e98b28121c7631080a1
habu <agunghabuu@gmail.com>
2026-01-14 21:44:19 +0700

    login page ui (title, 2 tf, button)

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -5,6 +5,38 @@ class Login extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 24),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Text(
+              'Login to proceed',
+              textAlign: TextAlign.center,
+              style: TextStyle(fontSize: 24),
+            ),
+            SizedBox(height: 16),
+            TextField(
+              decoration: InputDecoration(
+                labelText: 'Username',
+                border: OutlineInputBorder(),
+              ),
+            ),
+            SizedBox(height: 16),
+            TextField(
+              obscureText: true,
+              decoration: InputDecoration(
+                labelText: 'Password',
+                border: OutlineInputBorder(),
+              ),
+            ),
+            SizedBox(height: 16),
+            FilledButton(onPressed: () {}, child: Text('Login')),
+          ],
+        ),
+      ),
+    );
   }
 }
