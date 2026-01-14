404e8eabfd14e30244f69b1cd72b2629be8fd9c8
habu <agunghabuu@gmail.com>
2026-01-14 22:23:20 +0700

    add title & clickable card to open the camera in user dashboard page

--- lib/pages/user_dashboard.dart
+++ lib/pages/user_dashboard.dart
@@ -5,6 +5,38 @@ class UserDashboard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(title: Text('Welcome {User}')),
+      body: ListView(
+        padding: EdgeInsets.symmetric(horizontal: 16, vertical: 16),
+        children: [
+          InkWell(
+            onTap: () {},
+            borderRadius: BorderRadius.circular(16),
+            child: Container(
+              height: 220,
+              decoration: BoxDecoration(
+                border: Border.all(color: Colors.white24),
+                borderRadius: BorderRadius.circular(16),
+              ),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Icon(
+                    Icons.camera_alt_outlined,
+                    size: 48,
+                    color: Colors.white24,
+                  ),
+                  Text(
+                    'Tap to take photo',
+                    style: TextStyle(color: Colors.white24),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
