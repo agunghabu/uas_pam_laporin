dd1c1f1abb6d37a4479e1ce42cc70ff26b16ef60
habu <agunghabuu@gmail.com>
2026-01-17 05:58:19 +0700

    add extras menu on admin dashabord

--- lib/pages/admin_dashboard.dart
+++ lib/pages/admin_dashboard.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:uas_pam_laporin/provider/provider_theme.dart';
 
 import '../utils/widgets.dart';
 
@@ -13,6 +15,87 @@ class AdminDashboard extends StatelessWidget {
       child: Scaffold(
         appBar: AppBar(
           title: const Text('Laporin'),
+          actions: [
+            InkWell(
+              borderRadius: BorderRadius.circular(24),
+              onTap: () {
+                showDialog(
+                  context: context,
+                  builder: (context) => LAlertDialog(
+                    icon: Icons.category_outlined,
+                    title: 'Extra',
+                    iconColor: Theme.of(context).colorScheme.onSurfaceVariant,
+                    actions: [
+                      TextButton(onPressed: () => Navigator.pop(context), child: Text('Logout')),
+                      FilledButton(onPressed: () => Navigator.pop(context), child: Text('OK')),
+                    ],
+                    content: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Row(
+                          mainAxisSize: MainAxisSize.min,
+                          children: [
+                            Row(
+                              children: [
+                                Icon(Icons.person_outline_rounded, size: 16),
+                                SizedBox(width: 4),
+                                LText.bodyMedium(context, 'habuhenka'),
+                              ],
+                            ),
+                            Text(' | ', style: Theme.of(context).textTheme.bodyMedium),
+                            Row(
+                              children: [
+                                Icon(Icons.numbers_rounded, size: 16),
+                                SizedBox(width: 4),
+                                LText.bodyMedium(context, '3012310701'),
+                              ],
+                            ),
+                          ],
+                        ),
+                        SizedBox(height: 8),
+                        Divider(),
+                        Row(
+                          children: [
+                            Icon(Icons.dark_mode_outlined, size: 30),
+                            SizedBox(width: 16),
+                            LText.bodyMedium(context, 'Dark Mode'),
+                            Spacer(),
+                            Switch(
+                              value: context.watch<ProviderAppTheme>().darkMode,
+                              onChanged: (value) {
+                                context.read<ProviderAppTheme>().setDarkMode(value);
+                              },
+                            ),
+                          ],
+                        ),
+                        SizedBox(height: 16),
+                        Row(
+                          children: [
+                            Icon(Icons.color_lens_outlined, size: 30),
+                            SizedBox(width: 16),
+                            LDropdownMenu(
+                              onSelected: (value) => context.read<ProviderAppTheme>().setColorIndex(value ?? 0),
+                              menuHeight: 200,
+                              labelText: 'Color Theme',
+                              initialSelection: context.read<ProviderAppTheme>().colorIndex,
+                              dropdownMenuEntries: context.read<ProviderAppTheme>().colorDropdownItems,
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                );
+              },
+              child: Container(
+                padding: EdgeInsets.all(8),
+                decoration: BoxDecoration(shape: BoxShape.circle),
+                child: Icon(Icons.category_outlined, size: 26),
+              ),
+            ),
+            SizedBox(width: 8),
+          ],
           bottom: TabBar(
             tabs: [
               Tab(text: 'Pending', icon: Icon(Icons.warning_amber_rounded)),
