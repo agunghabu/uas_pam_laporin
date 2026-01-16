01f989f11a4aeb551d1970465ed52ba2d9b1cdc5
habu <agunghabuu@gmail.com>
2026-01-17 01:12:57 +0700

    copy template

--- lib/pages/user_dashboard.dart
+++ lib/pages/user_dashboard.dart
@@ -2,6 +2,7 @@ import 'dart:io';
 
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
+import 'package:uas_pam_laporin/utils/variables.dart';
 import 'package:uas_pam_laporin/utils/widgets.dart';
 
 import '../utils/helpers.dart';
@@ -30,6 +31,14 @@ class _UserDashboardState extends State<UserDashboard> {
       child: Scaffold(
         appBar: AppBar(
           title: Text('Welcome {User}'),
+          actions: [
+            TextButton(
+              onPressed: () {
+                setState(() => AppTheme.appBrightness = Brightness.light);
+              },
+              child: Icon(Icons.bedtime_outlined, size: 24),
+            ),
+          ],
           bottom: TabBar(
             tabs: [
               Tab(text: 'Submit Report', icon: Icon(Icons.add_circle_outline)),
@@ -208,7 +217,113 @@ class _UserDashboardState extends State<UserDashboard> {
                 ),
               ],
             ),
-            Center(child: Text('My Reports')),
+            ListView(
+              padding: EdgeInsets.all(16),
+              children: [
+                Container(
+                  decoration: BoxDecoration(
+                    border: Border.all(color: Theme.of(context).colorScheme.outline),
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: [
+                      SizedBox(
+                        height: 220,
+                        child: ClipRRect(
+                          borderRadius: BorderRadius.circular(12),
+                          child: Image.network(
+                            'https://www.siliconera.com/wp-content/uploads/2025/02/wuthering-waves-version-12.png',
+                            fit: BoxFit.cover,
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.fromLTRB(16, 16, 16, 8),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            Text('Broken AC in G3-R1', style: Theme.of(context).textTheme.headlineSmall),
+                            SizedBox(height: 8),
+                            Row(
+                              mainAxisSize: MainAxisSize.min,
+                              children: [
+                                Row(
+                                  children: [
+                                    Icon(Icons.person_outline_rounded, size: 16),
+                                    SizedBox(width: 4),
+                                    Text('habuhenka', style: Theme.of(context).textTheme.bodyMedium),
+                                  ],
+                                ),
+                                Text(' | ', style: Theme.of(context).textTheme.bodyMedium),
+                                Row(
+                                  children: [
+                                    Icon(Icons.calendar_today_outlined, size: 16),
+                                    SizedBox(width: 4),
+                                    Text('2024-06-15', style: Theme.of(context).textTheme.bodyMedium),
+                                  ],
+                                ),
+                                Text(' | ', style: Theme.of(context).textTheme.bodyMedium),
+                                Row(
+                                  children: [
+                                    Icon(Icons.access_time_outlined, size: 16),
+                                    SizedBox(width: 4),
+                                    Text('14:30', style: Theme.of(context).textTheme.bodyMedium),
+                                  ],
+                                ),
+                              ],
+                            ),
+                            Row(
+                              mainAxisSize: MainAxisSize.min,
+                              children: [
+                                Row(
+                                  children: [
+                                    Icon(Icons.location_on_outlined, size: 16),
+                                    SizedBox(width: 4),
+                                    Text('Campus A', style: Theme.of(context).textTheme.bodyMedium),
+                                  ],
+                                ),
+                                Text(' | ', style: Theme.of(context).textTheme.bodyMedium),
+                                Row(
+                                  children: [
+                                    Icon(Icons.apartment_outlined, size: 16),
+                                    SizedBox(width: 4),
+                                    Text('G3-R1', style: Theme.of(context).textTheme.bodyMedium),
+                                  ],
+                                ),
+                              ],
+                            ),
+                            SizedBox(height: 12),
+                            Text(
+                              'The AC has been leaking water for two days.',
+                              style: Theme.of(context).textTheme.bodySmall,
+                              textAlign: TextAlign.center,
+                            ),
+                            Divider(),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                              children: [
+                                TextButton.icon(
+                                  onPressed: () {},
+                                  label: Text('Reject'),
+                                  icon: Icon(Icons.close_rounded),
+                                ),
+                                FilledButton(onPressed: () {}, child: Text('See details')),
+                                TextButton.icon(
+                                  onPressed: () {},
+                                  label: Text('Accept'),
+                                  icon: Icon(Icons.check_rounded),
+                                ),
+                              ],
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
           ],
         ),
       ),
